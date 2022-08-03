1c6b2b101a0881b31831a18332b53606ac109529
Christophe <christophe.dame@camunda.com>
2022-08-03 11:07:11 +0200

    Read keycloak properties from application.yaml

--- src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -1,9 +1,12 @@
 package org.example.camunda.process.solution;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
-import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
 
 @SpringBootApplication
 @EnableZeebeClient
@@ -12,5 +15,10 @@ public class ProcessApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProcessApplication.class, args);
+  }
+  
+  @Bean
+  public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+      return new KeycloakSpringBootConfigResolver();
   }
 }
